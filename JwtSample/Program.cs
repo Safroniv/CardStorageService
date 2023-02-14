859a4ca8f0b344174f334736dc8ca73cd65f248e
Иван Сафронов <safroniv@mail.ru>
2023-02-14 20:17:20 +0300

    JobLsnSeqrDev02 red2

--- JwtSample/Program.cs
+++ JwtSample/Program.cs
@@ -7,7 +7,7 @@
         {
             Console.WriteLine("Enter user name:");
             string userName = Console.ReadLine();
-            Console.WriteLine("Enter user namepassword");
+            Console.WriteLine("Enter user password");
             string userPassword = Console.ReadLine();
             UserService userService = new UserService();
             string token = userService.Authentication(userName, userPassword);
